736fc77e5a57b83cb6ec406fd9831b406f5c23eb
IsDevOps <issyoseghale@gmail.com>
2024-07-15 09:42:39 +0000

    deploy

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
-# terraform {
-#   backend "gcs" {
-#     bucket  = "test-bucket557"  
-#     prefix  = "terraform/state"   
-#   }
-# }
+terraform {
+  backend "gcs" {
+    bucket  = "test-bucket557"  
+    prefix  = "terraform/state"   
+  }
+}
--- demo-app/app.py
+++ demo-app/app.py
@@ -1,27 +1,10 @@
 from flask import Flask
-import psycopg2
-import os
 
 app = Flask(__name__)
 
-def get_db_connection():
-    connection = psycopg2.connect(
-        dbname=os.environ['DB_NAME'],
-        user=os.environ['DB_USER'],
-        password=os.environ['DB_PASS'],
-        host=os.environ['DB_HOST']
-    )
-    return connection
-
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    cur = conn.cursor()
-    cur.execute('SELECT NOW()')
-    now = cur.fetchone()
-    cur.close()
-    conn.close()
-    return f"The current time is {now[0]}"
+    return 'WELCOME TO KREDI DEPLOYMENT TASK!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0')
